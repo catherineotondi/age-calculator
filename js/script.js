d4c901ed6e30363b2757102cd7b6cc1859c6a67f
catherineotondi <catherineotondi@gmail.com>
2022-03-05 17:30:55 +0300

    changed the input type to number in the index file
    changed the button format type in index file
    
    erased the validate function in the js file
    
    changed the max and min length in index file inputs
    
    added the style attibutes in the stylesheet file
    
    deleted the style tag from the header in the index file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-function Validate() {
-    var date=document.getElementById("date").value;
-    var month=document.getElementById("month").value;
-    var year=document.getElementById("year").value;
-    var century=document.getElementById("century").value;
-}
+// function Validate() {
+//     var date=document.getElementById("date").value;
+//     var month=document.getElementById("month").value;
+//     var year=document.getElementById("year").value;
+//     var century=document.getElementById("century").value;
+
+//     if (date=="" || month=="" || year==""|| century==""){
+//         alert("required");
+//         return false;
+//     }
+//     else{
+//         return true;
+//     }
+// }
