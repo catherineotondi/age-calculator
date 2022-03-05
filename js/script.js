550290bc11c4c3f1b9762a80475d68d4a0d8272a
catherineotondi <catherineotondi@gmail.com>
2022-03-05 14:58:55 +0300

    created a validate function in the js file
    edited the paragraph tags in the index file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function Validate() {
+    var date=document.getElementById("date").value;
+    var month=document.getElementById("month").value;
+    var year=document.getElementById("year").value;
+    var century=document.getElementById("century").value;
+}
