76aba398dd49fdde4af1f108859d59366dc128e2
catherineotondi <catherineotondi@gmail.com>
2022-03-06 14:04:08 +0300

    edited the logical opertors in the js file
    added the square brackets in the array digits

--- js/script.js
+++ js/script.js
@@ -23,48 +23,48 @@ document.getElementById("button").onclick=function akanName(){
 
     var weekDay= ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) %7;
 
-    if (weekDay=1 && male==1){
+    if (weekDay==0 && male==[0]){
         alert("Your Akan name is Kwasi")
     }
-    else if (weekDay =2 && male==2){
+    else if (weekDay==1 && male==[1]){
         alert("Your Akan name is Kwadwo")
     }
-    else if (weekDay =3 && male==3){
+    else if (weekDay==2 && male==[2]){
         alert("Your Akan name is Kwabena")
     }
-    else if (weekDay =4 && male==4){
+    else if (weekDay==3 && male==[3]){
         alert("Your Akan name is Kwaku")
     }
-    else if (weekDay =5 && male==5){
+    else if (weekDay==4 && male==[4]){
         alert("Your Akan name is Yaw")
     }
-    else if (weekDay =6 && male==6){
+    else if (weekDay==5 && male==[5]){
         alert("Your Akan name is Kofi")
     }
-    else if (weekDay =7 && male==7){
+    else if (weekDay==6 && male==[6]){
        alert("Your Akan name is Kwame")
     }
-    else if (weekDay =1 && female==1){
+    else if (weekDay==0 && female==[0]){
        alert("Your Akan name is AKosua")
     }
-    else if (weekDay =2 && female==2){
+    else if (weekDay==1 && female==[1]){
         alert("Your Akan name is Adwoa")
     }
-    else if (weekDay =3 && female==3){
+    else if (weekDay==2 && female==[2]){
         alert("Your Akan name is Abenaa")
      }
-     else if (weekDay =4 && female==4){
+     else if (weekDay==3 && female==[3]){
         alert("Your Akan name is Akua")
      }
-     else if (weekDay =5 && female==5){
+     else if (weekDay==4 && female==[4]){
         alert("Your Akan name is Yaw")
      }
-     else if (weekDay =6 && female==6){
+     else if (weekDay==5 && female==[5]){
         alert("Your Akan name is Afua")
      }
-     else if (weekDay =7 && female==7){
+     else if (weekDay==6 && female==[6]){
         alert("Your Akan name is Ama")
      }
-   
-        
-     }
+     else
+     alert("akan name not found")
+    }
